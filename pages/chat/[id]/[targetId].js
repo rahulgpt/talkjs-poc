48f29e3aaf1af852cc6f2311cddad53d33337550
Wai <wai1chung@gmail.com>
2022-02-06 14:27:08 +0800

    feat: Complete POC for wallet to wallet messaging

--- pages/chat/[id]/[targetId].js
+++ pages/chat/[id]/[targetId].js
@@ -1,93 +1,103 @@
-import React, { Component } from 'react';
-import Talk from 'talkjs';
-import { withRouter } from 'next/router';
+import React, { Component } from "react";
+import Talk from "talkjs";
+import { withRouter } from "next/router";
 
+//TODO:
+//how to prevent people from generating the chat id?
+//1. encrypting it using HMAC?
+//2. Use the API, the secret is in the backend and the user needs to authenticate in order to get the messages
+//The concept of private vs public chats?
 class Chat extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.inbox = undefined;
+  }
 
-        this.inbox = undefined;
-    }
+  componentDidMount() {
+    // Promise can be `then`ed multiple times
+    const pathname = window.location.pathname;
+    const query = pathname.split("/");
+    const id = query[2].toUpperCase();
+    const targetId = query[3].toUpperCase();
+    Talk.ready
+      .then(() => {
+        // const users = {
+        //   12345: {
+        //     name: "Aaron Jack",
+        //     email: "george@looney.net",
+        //     photoUrl: "https://randomuser.me/api/portraits/men/15.jpg",
+        //     welcomeMessage: "Hey there! How are you? :-)",
+        //   },
 
-    componentDidMount() {
-        // Promise can be `then`ed multiple times
-        Talk.ready
-            .then(() => {
-                const { id, targetId } = this.props.router.query
+        //   54321: {
+        //     name: "Albert Johnston",
+        //     email: "ronald@teflon.com",
+        //     photoUrl: "https://randomuser.me/api/portraits/men/42.jpg",
+        //     welcomeMessage: "Hey there! Love to chat :-)",
+        //   },
+        // };
 
-                const users = {
-                    '12345': {
-                        name: "Aaron Jack",
-                        email: "george@looney.net",
-                        photoUrl: "https://randomuser.me/api/portraits/men/15.jpg",
-                        welcomeMessage: "Hey there! How are you? :-)"
-                    },
+        const me = new Talk.User({
+          id,
+          name: id,
+          email: "email1@gmail.com",
+          //   name: users[id].name,
+          //   email: users[id].email,
+          //   photoUrl: users[id].photoUrl,
+          //   welcomeMessage: users[id].welcomeMessage,
+        });
 
-                    '54321': {
-                        name: "Albert Johnston",
-                        email: "ronald@teflon.com",
-                        photoUrl: "https://randomuser.me/api/portraits/men/42.jpg",
-                        welcomeMessage: "Hey there! Love to chat :-)"
-                    }
-                }
+        const other = new Talk.User({
+          // id: "54321",
+          id: targetId,
+          name: targetId,
+          email: "email3@gmail.com",
+          //   name: users[targetId].name,
+          //   email: users[targetId].email,
+          //   photoUrl: users[targetId].photoUrl,
+          //   welcomeMessage: users[targetId].welcomeMessage,
+        });
 
-                const me = new Talk.User({
-                    id,
-                    name: users[id].name,
-                    email: users[id].email,
-                    photoUrl: users[id].photoUrl,
-                    welcomeMessage: users[id].welcomeMessage
-                });
-
-                const other = new Talk.User({
-                    // id: "54321",
-                    id,
-                    name: users[targetId].name,
-                    email: users[targetId].email,
-                    photoUrl: users[targetId].photoUrl,
-                    welcomeMessage: users[targetId].welcomeMessage
-                });
-
-                if (!window.talkSession) {
-                    window.talkSession = new Talk.Session({
-                        appId: "t6NhgaOQ",
-                        me: me
-                    });
-                }
-
-
-
-                // You control the ID of a conversation. oneOnOneId is a helper method that generates
-                // a unique conversation ID for a given pair of users. 
-                const conversationId = Talk.oneOnOneId(me, other);
-                console.log(conversationId)
-
-                const conversation = window.talkSession.getOrCreateConversation(conversationId);
-                conversation.setParticipant(me);
-                conversation.setParticipant(other);
-
-                this.inbox = window.talkSession.createInbox({
-                    selected: conversation
-                });
-                this.inbox.mount(this.container);
-
-            })
-            .catch(e => console.error(e));
-    }
-
-    componentWillUnmount() {
-        if (this.inbox) {
-            this.inbox.destroy();
+        if (!window.talkSession) {
+          window.talkSession = new Talk.Session({
+            appId: "t6NhgaOQ",
+            me: me,
+          });
         }
-    }
 
-    render() {
+        // You control the ID of a conversation. oneOnOneId is a helper method that generates
+        // a unique conversation ID for a given pair of users.
+        const conversationId = Talk.oneOnOneId(me, other);
 
-        return (<span>
-            <div style={{ height: '500px' }} ref={c => this.container = c}>Loading...</div>
-        </span>);
+        const conversation =
+          window.talkSession.getOrCreateConversation(conversationId);
+        conversation.setParticipant(me);
+        conversation.setParticipant(other);
+
+        this.inbox = window.talkSession.createInbox({
+          selected: conversation,
+        });
+        this.inbox.mount(this.container);
+      })
+      .catch((e) => console.error(e));
+  }
+
+  componentWillUnmount() {
+    if (this.inbox) {
+      this.inbox.destroy();
     }
+  }
+
+  render() {
+    return (
+      <span>
+        <div style={{ height: "500px" }} ref={(c) => (this.container = c)}>
+          Loading...
+        </div>
+      </span>
+    );
+  }
 }
 
 export default withRouter(Chat);
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,171 @@
-import React, { Component } from 'react';
-import styles from '../styles/Home.module.css';
-import { withRouter } from 'next/router';
+import React, { useState } from "react";
+import { Button, List, Input, Typography } from "antd";
+import "antd/dist/antd.css";
+import moralis from "lib/moralis";
+import { useRouter } from "next/router";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userId: '',
-      targetId: '',
+const { Search } = Input;
+
+//TODO:
+//Send to multiple people
+//Search token by SYMBOL
+const LoggedOutState = () => {
+  /* Authentication code */
+  async function login() {
+    let user = moralis.User.current();
+    if (!user) {
+      user = await moralis
+        .authenticate({
+          signingMessage: "Log in using Moralis",
+        })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+          window.location.reload();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUserIdChange = this.handleUserIdChange.bind(this);
-    this.handleTargetIdChange = this.handleTargetIdChange.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  return (
+    <div>
+      <Button onClick={login} type="primary">
+        Log In
+      </Button>
+    </div>
+  );
+};
 
-    this.props.router.push(`/chat/${this.state.userId}/${this.state.targetId}`);
+const LogOut = () => {
+  async function logOut() {
+    await moralis.User.logOut();
+    console.log("logged out");
+    window.location.reload();
   }
+  return (
+    <div>
+      <Button onClick={logOut} type="dashed">
+        Log Out
+      </Button>
+    </div>
+  );
+};
 
-  handleUserIdChange(e) {
-    this.setState({ userId: e.target.value });
-  }
+//add debounce
+const SearchTokens = () => {
+  // "0x3883f5e181fccaf8410fa61e12b59bad963fb645";
+  const [tokenHolders, setTokenHolders] = useState([]);
+  const router = useRouter();
+  const user = moralis.User.current();
+  const onSearch = async (value) => {
+    const res = await moralis.Plugins.covalent.getBlockTokenHolders({
+      chainId: 1,
+      contractAddress: value,
+      blockHeight: "latest", //how to get latest blockheight
+      // startingBlock: "",
+      // endingBlock: "",
+      // pageNumber: 0,
+      // pageSize: "",
+      // quoteCurrency: "USD",
+    });
+    const data = res.data.items;
+    const tokenHoldersPubKeys = data.map((each) => each.address);
+    // setTokenHolders(tokenHoldersPubKeys);
+    setTokenHolders([
+      "0x5401E082ED965b134a27Ef3e0b48a7B4d89f426c",
+      "0xB8268407A0B6b2292BB9cd61663AAaB6175A97dA",
+    ]);
+  };
 
-  handleTargetIdChange(e) {
-    this.setState({ targetId: e.target.value });
-  }
+  const onOpenChatWithTokenHolder = (tokenholder) => () => {
+    router.push(`/chat/${user.get("ethAddress")}/${tokenholder}`);
+  };
+  return (
+    <>
+      <LogOut />
+      <Search placeholder="input search text" onSearch={onSearch} enterButton />
+      <List
+        bordered
+        dataSource={tokenHolders}
+        renderItem={(item) => (
+          <List.Item
+            style={{ cursor: "pointer" }}
+            onClick={onOpenChatWithTokenHolder(item)}
+          >
+            <Typography.Text mark>{item}</Typography.Text>
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <h1>Welcome</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Your Id:
-            <input type="text" name="name" value={this.state.value} onChange={this.handleUserIdChange} />
-          </label>
+const Home = () => {
+  const user = moralis.User.current();
 
-          <label>
-            Target Id:
-            <input type="text" name="name" value={this.state.value} onChange={this.handleTargetIdChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
-}
+  return user ? <SearchTokens /> : <LoggedOutState />;
+};
 
-export default withRouter(App);
+export default Home;
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       userId: "",
+//       targetId: "",
+//     };
+
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleUserIdChange = this.handleUserIdChange.bind(this);
+//     this.handleTargetIdChange = this.handleTargetIdChange.bind(this);
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+
+//     this.props.router.push(`/chat/${this.state.userId}/${this.state.targetId}`);
+//   }
+
+//   handleUserIdChange(e) {
+//     this.setState({ userId: e.target.value });
+//   }
+
+//   handleTargetIdChange(e) {
+//     this.setState({ targetId: e.target.value });
+//   }
+
+//   render() {
+//     return (
+//       <div className={styles.container}>
+//         <h1>Welcome</h1>
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Your Id:
+//             <input
+//               type="text"
+//               name="name"
+//               value={this.state.value}
+//               onChange={this.handleUserIdChange}
+//             />
+//           </label>
+
+//           <label>
+//             Target Id:
+//             <input
+//               type="text"
+//               name="name"
+//               value={this.state.value}
+//               onChange={this.handleTargetIdChange}
+//             />
+//           </label>
+//           <input type="submit" value="Submit" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default withRouter(App);
